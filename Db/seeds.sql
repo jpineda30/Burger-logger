4ad5865b810363a18dff585b46115c554f08191d
pepe30 <jose.pinedaesquer@gmail.com>
2021-01-11 15:45:34 -0600

    added sql files

---
+++ Db/seeds.sql
@@ -0,0 +1,3 @@
+insert into burgers (burger_name) values ("Cheese burger");
+insert into burgers (burger_name) values ("Mooshroom burger");
+insert into burgers (burger_name) values ("Beacon burger");
