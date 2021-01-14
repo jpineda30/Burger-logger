953917a8f01bd4a68a77a909d130ffa90f792f0d
pepe30 <jose.pinedaesquer@gmail.com>
2021-01-13 18:11:44 -0600

    Corrected code for refreshing on create

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -41,14 +41,8 @@ router.post("/api/burgers", function(req, res) {
   
 
   burger.insertOne(req.body.type,req.body.burger, function(result) {
-
-    if (result.changedRows == 0) {
-      // If no rows were changed, then the ID must not exist, so 404
-      return res.status(404).end();
-    } else {
-      res.status(200).end();
-    }
-  });
+      res.send("success");
+      });
 
 
 });
